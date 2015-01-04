900a5f222e015a86fa70a45b5c8ef6659f2decc1
Chanwit Kaewkasi <chanwit@gmail.com>
2015-01-04 16:01:54 +0700

    remove unused ui-select

--- static/app.js
+++ static/app.js
@@ -2,46 +2,45 @@ var app = angular.module('app', [/*'ui.select'*/]);
 
 app.controller('AppCtrl', function ($scope, $http) {
 
-  $scope.refresh = function() {
-    $http.get("/filenames").
-    success(function(data, status, header, config) {
-      $scope.filenames = data['filenames'];
-    });
-  }
+    $scope.refresh = function() {
+        $http.get("/filenames").
+        success(function(data, status, header, config) {
+            $scope.filenames = data['filenames'];
+        });
+    }
 
-  $scope.refresh()
+    $scope.refresh()
 
-  $scope.doSave = function() {
-    var wk = window.prompt("Save as", $scope.workspace);
-    var dom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-    var xmlText = Blockly.Xml.domToPrettyText(dom);
+    $scope.doSave = function() {
+        var wk = window.prompt("Save as", $scope.workspace);
+        var dom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+        var xmlText = Blockly.Xml.domToPrettyText(dom);
 
-    $http.post("/save",
-       "xml=" + xmlText + "&" + "workspace=" + wk,
-       {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).
-    success(function(data, status, header, config) {
-      $scope.refresh()
-    });
-  };
+        $http.post("/save", "xml=" + xmlText + "&" + "workspace=" + wk,
+            {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).
+        success(function(data, status, header, config) {
+            $scope.refresh()
+        });
+    };
 
-  $scope.doLoad = function() {
-    $http.get("/load/" + $scope.workspace).
-    success(function(data, status, header, config) {
-      Blockly.mainWorkspace.clear();
-      var xml = Blockly.Xml.textToDom(data);
-      Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
-    });
-  };
+    $scope.doLoad = function() {
+        $http.get("/load/" + $scope.workspace).
+        success(function(data, status, header, config) {
+            Blockly.mainWorkspace.clear();
+            var xml = Blockly.Xml.textToDom(data);
+            Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+        });
+    };
 
-  $scope.doClear = function() {
-   Blockly.mainWorkspace.clear();
-  };
+    $scope.doClear = function() {
+        Blockly.mainWorkspace.clear();
+    };
 
-  $scope.doDeploy = function() {
-    $http.post("/deploy/" + $scope.workspace).
-    success(function(data, status, header, config) {
-      alert(data);
-    });
-  };
+    $scope.doDeploy = function() {
+        $http.post("/deploy/" + $scope.workspace).
+        success(function(data, status, header, config) {
+            alert(data);
+        });
+    };
 
 });

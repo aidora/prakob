60e6083095d6437c2c6d83b823098267f8c5c2e0
Chanwit Kaewkasi <chanwit@gmail.com>
2015-01-05 21:38:16 +0700

    clean up

--- static/monitor.js
+++ static/monitor.js
@@ -3,7 +3,6 @@ var app = angular.module('monitor', [/*'ui.select'*/]);
 app.controller('MonitorCtrl', function ($scope, $http) {
 
     var url = "http://192.168.1.3:8888/";
-
     $scope.containers = []
 
     $scope.updateContainers = function() {
@@ -15,6 +14,9 @@ app.controller('MonitorCtrl', function ($scope, $http) {
 
     };
 
+    setTimeout($scope.updateContainers, 500);
+    setInterval($scope.updateContainers, 1500);
+
     $scope.pause = function(containerId, status) {
         var paused = status.endsWith("(Paused)");
         var cmd = paused ? "unpause" : "pause";
@@ -23,13 +25,13 @@ app.controller('MonitorCtrl', function ($scope, $http) {
         success(function(data){
             $scope.updateContainers();
         });
-    }
+    };
 
     $scope.kill = function(containerId) {
         $http.post(url + "containers/" + containerId + "/kill").
         success(function(data){
             $scope.updateContainers();
         });
-    }
+    };
 
 })

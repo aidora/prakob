f0a80f72ac832b683c92df8f5666e3ecbb2667b0
Chanwit Kaewkasi <chanwit@gmail.com>
2015-01-05 21:28:25 +0700

    implement pause and kill

---
+++ static/monitor.js
@@ -0,0 +1,35 @@
+var app = angular.module('monitor', [/*'ui.select'*/]);
+
+app.controller('MonitorCtrl', function ($scope, $http) {
+
+    var url = "http://192.168.1.3:8888/";
+
+    $scope.containers = []
+
+    $scope.updateContainers = function() {
+
+        $http.get(url + "containers/json").
+        success(function(data){
+            $scope.containers = data;
+        });
+
+    };
+
+    $scope.pause = function(containerId, status) {
+        var paused = status.endsWith("(Paused)");
+        var cmd = paused ? "unpause" : "pause";
+
+        $http.post(url + "containers/" + containerId + "/" + cmd).
+        success(function(data){
+            $scope.updateContainers();
+        });
+    }
+
+    $scope.kill = function(containerId) {
+        $http.post(url + "containers/" + containerId + "/kill").
+        success(function(data){
+            $scope.updateContainers();
+        });
+    }
+
+})
